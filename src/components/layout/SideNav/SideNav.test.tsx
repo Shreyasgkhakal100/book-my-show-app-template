825a8e66e52b60fbdb77cffd56738aa9b5fa7f48
Shreyas Khakal <shreyasgkhakal100@gmail.com>
2022-05-31 18:26:49 +0530

    Added test cases for MoviesTABLE

--- src/components/layout/SideNav/SideNav.test.tsx
+++ src/components/layout/SideNav/SideNav.test.tsx
@@ -1,11 +1,11 @@
-const mockHistoryPush = jest.fn();
+// const mockHistoryPush = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-    ...(jest.requireActual('react-router-dom') as any),
-    useHistory: () => ({
-        push: mockHistoryPush
-    })
-}));
+// jest.mock('react-router-dom', () => ({
+//     ...(jest.requireActual('react-router-dom') as any),
+//     useHistory: () => ({
+//         push: mockHistoryPush
+//     })
+// }));
 
 describe('SideNav', () => {
     it('should call history.push on click on dashboard', async () => {
